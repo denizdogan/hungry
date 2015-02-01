0fe468b79d3b731e6cf7ad4274d71f0e1ec0cc20
Deniz Dogan <deniz@dogan.se>
2015-02-01 22:08:03 +0100

    Adding project, requirements and tests

---
+++ hungry/__init__.py
@@ -0,0 +1,16 @@
+def eat(*ex, **kwargs):
+    error_handler = kwargs.get('error_handler', None)
+    error_value = kwargs.get('error_value', None)
+
+    def inner(func):
+        def wrapper(*args, **kw):
+            try:
+                return func(*args, **kw)
+            except ex as e:
+                if error_handler is not None:
+                    return error_handler(e, *args, **kw)
+                return error_value
+
+        return wrapper
+
+    return inner
---
+++ tests/test_eat.py
@@ -0,0 +1,23 @@
+import pytest
+from hungry import eat
+
+
+def test_catch_one_exception():
+    def f(val):
+        return int(val[1])
+
+    # should raise IndexError
+    with pytest.raises(IndexError):
+        f([1])
+
+    # should NOT raise any exceptions
+    f([1, 2])
+
+    # decorate 'foo'
+    foo = eat(IndexError)(f)
+
+    # this time, the IndexError should be eaten
+    foo([1])
+
+    # ...but not ValueError
+    foo(['foo'])
